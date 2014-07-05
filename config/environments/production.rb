05e2d5fcbc8555a77ed9f792c5a301fd87ad9891
Lynn Dylan Hurley <lynn.dylan.hurley@gmail.com>
2014-07-04 20:31:58 -0500

    set mailer host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,6 +73,8 @@ Rails.application.configure do
   }
   ActionMailer::Base.delivery_method = :smtp
 
+  config.action_mailer.default_url_options = { :host => 'devise-token-auth-demo.herokuapp.com' }
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
