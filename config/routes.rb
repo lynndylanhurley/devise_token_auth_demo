9c83a83b69b658a76ca87d6140ac9705c44f60e3
Lynn Dylan Hurley <lynn.dylan.hurley@gmail.com>
2014-07-16 22:58:16 -0500

    make user the default devise_scope

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
+  mount_devise_token_auth_for 'User', at: '/auth'
   mount_devise_token_auth_for 'Mang', at: 'bong'
 
-  mount_devise_token_auth_for 'User', at: '/auth'
-  as :user do
-    # Define routes for User within this block.
-  end
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-  mount DeviseTokenAuth::Engine => "/auth"
-
   get "demo/members_only", to: "demo#members_only"
+
+  as :mang do
+    get "demo/members_only", to: "demo#members_only"
+  end
 end
