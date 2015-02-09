fac49d577238a39a6f67a804a1e445208fe5c6f6
Lynn Hurley <lynn.dylan.hurley@gmail.com>
2015-02-08 23:30:39 -0600

    update routes with new mount format

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: '/auth'
-  mount_devise_token_auth_for 'Mang', at: '/mangs'
+  mount_devise_token_auth_for 'User', at: 'auth'
+  mount_devise_token_auth_for 'Mang', at: 'mangs'
 
   # this route will authorize visitors using the User class
   get 'demo/members_only', to: 'demo_user#members_only'
